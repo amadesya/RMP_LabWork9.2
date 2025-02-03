e938ac07b77d7cbbbdf742820021efed16a896da
amadesya <154593967+amadesya@users.noreply.github.com>
2025-02-03 20:11:00 +0300

    Update Task2.kt

--- src/Task2.kt
+++ src/Task2.kt
@@ -1,3 +1,35 @@
-fun main() {
-    println("Hello World!")
+fun main(){
+    var numbers: MutableList<Int> = mutableListOf()
+    println("Введите количество элементов списка")
+    
+    var n = readln().toInt()
+    if (n == null || n < 0)
+        println("Введено неверное количество элементов")
+
+    for(i in 1..n) {
+        println("Введите элемент ${i}")
+        var element = readln().toInt()
+        if (!(element is Int))
+            println("Элемент не добавлен")
+        else
+            numbers.add(element)
+    }
+
+    //через indexOf
+    println("Индекс элемента, значение которого равно 100: ${numbers.indexOf(100)} ")
+    //через sum
+    println("Сумма элементов списка: ${numbers.sum()}")
+    //через average
+    println("Среднее значение элементов коллекции: ${numbers.average()}")
+
+    //через any
+    if (numbers.any{ it >= 0}){
+        println("Не все числа больше нуля")
+    }
+    else{
+        println("Все числа больше нуля")
+    }
+
+    //через filterNot
+    println("Список нечётных чисел: ${numbers.filterNot { it % 2 == 0}}")
 }
